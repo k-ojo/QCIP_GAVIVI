069510ba95ff83ab25392ce16b595c19363c3b4d
kojo <gideonad4@gmail.com>
2025-09-28 11:16:43 +0000

    added

---
+++ 1.0_versioon_check.py
@@ -0,0 +1,5 @@
+from qiskit import __version__ as qiskit_version
+from qiskit_ibm_runtime import __version__ as qiskit_ibm_runtime_version
+
+print("Qiskit version:", qiskit_version)
+print("Qiskit IBM Runtime version:", qiskit_ibm_runtime_version)
